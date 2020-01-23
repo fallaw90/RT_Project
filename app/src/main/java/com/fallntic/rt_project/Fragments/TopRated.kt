e581d049b9de243dd6b61f8c9b81745b8e51f1ab
fallaw90 <37890292+fallaw90@users.noreply.github.com>
2020-01-23 16:15:56 -0500

    Top rated done

--- app/src/main/java/com/fallntic/rt_project/Fragments/TopRated.kt
+++ app/src/main/java/com/fallntic/rt_project/Fragments/TopRated.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.fallntic.rt_project.Model.Card
@@ -18,21 +20,31 @@ import kotlinx.android.synthetic.main.fragment_top__rated_.*
 class TopRated : Fragment() {
 
     var listCard = ArrayList<Card>()
-    var card = Card()
-    var card2 = Card()
+    var score1 = 0
+    var score2 = 0
+
+    lateinit var textView1: TextView
+    lateinit var textView2: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        val view = inflater.inflate(R.layout.fragment_top__rated_, container, false)
+
+        textView1 = view.findViewById(R.id.score1)
+        textView2 = view.findViewById(R.id.score2)
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_top__rated_, container, false)
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
         for (i in 2..14) {
+            val card = Card()
             card.name = "card" + i
             card.value = i
             listCard.add(card)
@@ -42,13 +54,26 @@ class TopRated : Fragment() {
 
         text.setOnClickListener {
 
-            card = listCard.removeAt(listCard.size - 1)
-            showCard(card.name, ImageView1)
+            val tempCard1: Card
+            val tempCard2: Card
 
-            card2 = listCard.removeAt(listCard.size - 2)
-            showCard(card2.name, ImageView2)
+            if (listCard.size >= 2) {
+                tempCard1 = listCard.removeAt(listCard.size - 1)
+                showCard(tempCard1.name, ImageView1)
 
+                tempCard2 = listCard.removeAt(listCard.size - 1)
+                showCard(tempCard2.name, ImageView2)
 
+                if(tempCard1.value > tempCard2.value){
+                    score1++
+                }
+                else{
+                    score2++
+                }
+
+                textView1.text = score1.toString()
+                textView2.text = score2.toString()
+            }
         }
     }
 
@@ -56,9 +81,7 @@ class TopRated : Fragment() {
         val card_id = resources.getIdentifier(card_name, "drawable", context?.packageName)
         Glide.with(this)
             .load(card_id)
-            .placeholder(R.drawable.back)
-            .centerCrop()
+            .centerInside()
             .into(imageView)
     }
-
 }
