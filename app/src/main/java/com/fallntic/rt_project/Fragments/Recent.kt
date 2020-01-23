f292f1478ed5e9003d83cfea874f26d9856efa6a
Jaydeep789 <shetyejaydeep@gmail.com>
2020-01-23 13:33:49 -0500

    Working Navigation View

---
+++ app/src/main/java/com/fallntic/rt_project/Fragments/Recent.kt
@@ -0,0 +1,26 @@
+package com.fallntic.rt_project.Fragments
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.fallntic.rt_project.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class Recent : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_recent_, container, false)
+    }
+
+
+}
--- app/src/main/java/com/fallntic/rt_project/MainActivity.kt
+++ app/src/main/java/com/fallntic/rt_project/MainActivity.kt
@@ -2,11 +2,41 @@ package com.fallntic.rt_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.fallntic.rt_project.Fragments.MostViewed
+import com.fallntic.rt_project.Fragments.Recent
+import com.fallntic.rt_project.Fragments.TopRated
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNavigationView : BottomNavigationView
+    private lateinit var fragment: Fragment
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView = findViewById(R.id.bottom_nav)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(itemListener)
+
+    }
+
+    private var itemListener = BottomNavigationView.OnNavigationItemSelectedListener {menuItem ->
+        when(menuItem.itemId){
+            R.id.top_rated -> {
+                 fragment = TopRated()
+            }
+            R.id.most_viewed -> {
+                fragment = MostViewed()
+            }
+            R.id.recents -> {
+                fragment = Recent()
+            }
+        }
+        supportFragmentManager.beginTransaction().replace(R.id.container,fragment).commit()
+        return@OnNavigationItemSelectedListener true
     }
 }
