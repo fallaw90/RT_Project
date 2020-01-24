b0820d4461d50347d900c6d68362148095dc4d84
fallaw90 <37890292+fallaw90@users.noreply.github.com>
2020-01-24 13:20:09 -0500

    Recent Final Working commit

--- app/src/main/java/com/fallntic/rt_project/Fragments/MostViewed.kt
+++ app/src/main/java/com/fallntic/rt_project/Fragments/MostViewed.kt
@@ -2,12 +2,14 @@ package com.fallntic.rt_project.Fragments
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.FrameLayout
+import android.widget.LinearLayout
+import androidx.fragment.app.Fragment
 import com.fallntic.rt_project.R
+
 
 /**
  * A simple [Fragment] subclass.
@@ -19,8 +21,19 @@ class MostViewed : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_most__viewed_, container, false)
+        val view = inflater.inflate(R.layout.fragment_most__viewed_, container, false)
+
+        //val layout: FrameLayout = view.findViewById(R.id.frame_layout)
+
+        /*val params = layout.getLayoutParams() as FrameLayout.LayoutParams
+        params.setMargins(0, 100, 0, 0)
+        layout.setLayoutParams(params)*/
+
+        return view
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
 
+    }
 }
